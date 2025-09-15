889cbe6e7babede179d8880dc77f895a60deea70
alvariitoSW <alva00003@gmail.com>
2025-09-16 01:31:11 +0200

    banner del mapa del home

--- src/components/ui/ZoneServiceModal.tsx
+++ src/components/ui/ZoneServiceModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function ZoneServiceModal() {
   const [isModalOpen, setIsModalOpen] = useState(false);
